63fa3626ac42f2fe7c8ac8bbefb26cc4411a7fd0
adnan <adnan@gmail.com>
2021-03-06 17:30:29 -0800

    new file added

---
+++ app/Http/Middleware/CustomerMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CustomerMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(auth()->user()->user_type == "customer") {
+            return $next($request);
+        }
+        else{
+            return redirect()->back();
+        }
+    }
+}
---
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Providers\RouteServiceProvider;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RedirectIfAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  ...$guards
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, $guard = null)
+    {
+        if(Auth::check()){
+            if(auth()->user()->user_type == "admin"){
+                return redirect("/home");
+            }
+            elseif(auth()->user()->user_type == "customer") {
+                return redirect('/customer/dashboard');
+            }
+        }
+
+        return $next($request);
+    }
+}
